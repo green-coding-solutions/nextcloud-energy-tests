94bb3a2dfcd51b1d68e59dcb87e3458d45bf0e92
Arne Tarara <arne@datafuse.de>
2025-01-06 17:01:34 +0800

    Call must be sudo

--- run-tests.sh
+++ run-tests.sh
@@ -7,36 +7,36 @@ source ~/Sites/green-coding/green-metrics-tool/venv/bin/activate
 
 for i in {1..10}; do
    sync
-   /usr/sbin/sysctl -w vm.drop_caches=3
+   sudo /usr/sbin/sysctl -w vm.drop_caches=3
    python3 ~/Sites/green-coding/green-metrics-tool/runner.py --uri ~/Sites/green-coding/nextcloud-energy-tests --filename usage_scenario-mariadb-calendar-firefox.yml --dev-no-optimizations --allow-unsafe --name "Nextcloud Blue Angel - Calendar #${i}"
 done
 
 for i in {1..10}; do
    sync
-   /usr/sbin/sysctl -w vm.drop_caches=3
+   sudo /usr/sbin/sysctl -w vm.drop_caches=3
    python3 ~/Sites/green-coding/green-metrics-tool/runner.py --uri ~/Sites/green-coding/nextcloud-energy-tests --filename usage_scenario-mariadb-contacts-firefox.yml --dev-no-optimizations --allow-unsafe --name "Nextcloud Blue Angel - Contacts #${i}"
 done
 
 for i in {1..10}; do
     sync
-    /usr/sbin/sysctl -w vm.drop_caches=3
+    sudo /usr/sbin/sysctl -w vm.drop_caches=3
     python3 ~/Sites/green-coding/green-metrics-tool/runner.py --uri ~/Sites/green-coding/nextcloud-energy-tests --filename usage_scenario-mariadb-files-firefox.yml --dev-no-optimizations --allow-unsafe --name "Nextcloud Blue Angel - Files #${i}"
 done
 
 for i in {1..10}; do
     sync
-    /usr/sbin/sysctl -w vm.drop_caches=3
+    sudo /usr/sbin/sysctl -w vm.drop_caches=3
     python3 ~/Sites/green-coding/green-metrics-tool/runner.py --uri ~/Sites/green-coding/nextcloud-energy-tests --filename usage_scenario-mariadb-docs-firefox.yml --dev-no-optimizations --allow-unsafe --name "Nextcloud Blue Angel - Docs #${i}"
 done
 
 for i in {1..10}; do
     sync
-    /usr/sbin/sysctl -w vm.drop_caches=3
+    sudo /usr/sbin/sysctl -w vm.drop_caches=3
     python3 ~/Sites/green-coding/green-metrics-tool/runner.py --uri ~/Sites/green-coding/nextcloud-energy-tests --filename usage_scenario-mariadb-video-firefox.yml --dev-no-optimizations --allow-unsafe --name "Nextcloud Blue Angel - Video #${i}"
 done
 
 for i in {1..10}; do
     sync
-    /usr/sbin/sysctl -w vm.drop_caches=3
+    sudo /usr/sbin/sysctl -w vm.drop_caches=3
     python3 ~/Sites/green-coding/green-metrics-tool/runner.py --uri ~/Sites/green-coding/nextcloud-energy-tests --filename usage_scenario-mariadb-talk-firefox.yml --dev-no-optimizations --allow-unsafe --name "Nextcloud Blue Angel - Talk #${i}"
 done
